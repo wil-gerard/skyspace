840596070e4c1b0e18fb5d757068f76d37754a0c
Dan A <andersondanc@gmail.com>
2021-03-04 00:25:06 -0600

    Foreground now cycles between foreground-start and foreground-end every 5 minutes

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
+const foreground = document.querySelector('#foreground');
+const interval = 5 * 60 * 1000; //5 minutes in milliseconds
 
+function cycleForegroundClass() {
+    foreground.classList.toggle('foreground-start');
+    foreground.classList.toggle('foreground-end');
+    window.setTimeout(cycleForegroundClass, interval);
+}
+
+cycleForegroundClass();
