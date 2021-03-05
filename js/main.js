97f0db099933c19964cb470ac6d3b9674121c93d
wil-gerard <williamrgerard@gmail.com>
2021-03-04 23:41:53 -0600

    changed to a 30s interval for faster effect testing

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const foreground = document.querySelector('#foreground');
-const interval = 5 * 60 * 1000; //5 minutes in milliseconds
+const interval = 1 * 30 * 1000; //30 seconds in milliseconds
 
 function cycleForegroundClass() {
     foreground.classList.toggle('foreground-start');
@@ -8,3 +8,21 @@ function cycleForegroundClass() {
 }
 
 cycleForegroundClass();
+
+// class IntervalTimer {
+//     constructor(interval) {
+//         this.interval = interval;
+//     }
+//     get getInterval() {
+//         return this.interval
+//     }
+//     cycleForegroundClass() {
+//         let foreground = document.querySelector('foreground');
+//         foreground.classList.toggle('foreground-start');
+//         foreground.classList.toggle('foreground-end');
+//         window.setTimeout(cycleForegroundClass, interval)
+//     }
+// }
+
+// let foregroundIntervalTimer = new IntervalTimer((30 * 1000))
+// foregroundIntervalTimer.cycleForegroundClass()
